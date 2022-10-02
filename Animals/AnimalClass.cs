c590fff5f3759d892e0a558b8eed28cc0cb4a2c2
Rodrigo Merlone <alestori7@gmail.com>
2022-10-02 20:15:05 -0300

    Add animal classes as an individual class

---
+++ Animals/AnimalClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals {
+    internal class AnimalClass {
+		public AnimalClass() {
+			InitializeClasses();
+		}
+
+		private List<string> animalClasses;
+
+		public List<string> AnimalClasses {
+			get { return animalClasses; }
+		}
+
+		public void AddAnimalClass(string animalClass) {
+			AnimalClasses.Add(animalClass);
+		}
+
+		private void InitializeClasses() {
+			animalClasses = new List<string>();
+		}
+	}
+}
