e4c6d31b982b03f3f7f870880cf6d1f828bd262d
Rodrigo Merlone <alestori7@gmail.com>
2022-10-03 15:44:23 -0300

    simplifies AnimalClasses class

--- Animals/AnimalClass.cs
+++ Animals/AnimalClass.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Animals {
+﻿namespace Animals {
     internal class AnimalClass {
-		public AnimalClass() {
-			InitializeClasses();
-		}
+        public AnimalClass() {
+            InitializeClasses();
+        }
 
-		private List<string> animalClasses;
+        public List<string> AnimalClasses { get; set; }
 
-		public List<string> AnimalClasses {
-			get { return animalClasses; }
-		}
+        public void AddAnimalClass(string animalClass) {
+            AnimalClasses.Add(animalClass);
+        }
 
-		public void AddAnimalClass(string animalClass) {
-			AnimalClasses.Add(animalClass);
-		}
-
-		private void InitializeClasses() {
-			animalClasses = new List<string>();
-		}
-	}
+        private void InitializeClasses() {
+            AnimalClasses = new List<string>();
+        }
+    }
 }
